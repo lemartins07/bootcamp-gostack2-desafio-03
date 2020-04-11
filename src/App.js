9f98fbe7036e8beaef90cde4e5d239920834908e
Leandro Martins <leandro.martins.85@gmail.com>
2020-04-11 19:11:59 -0300

    Remover repositórios

--- src/App.js
+++ src/App.js
@@ -25,7 +25,8 @@ function App() {
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
+    setRepositories(repositories.filter((repository) => repository.id !== id));
   }
 
   return (
@@ -34,7 +35,9 @@ function App() {
         {repositories.map((repository) => (
           <li key={repository.id}>
             {repository.title}
-            <button onClick={() => handleRemoveRepository(1)}>Remover</button>
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
           </li>
         ))}
       </ul>
